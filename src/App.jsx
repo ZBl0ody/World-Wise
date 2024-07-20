e0ea9bc1c1adfea5dfe677e7a52bd07fb86e7fc4
ZBl0ody <boody.15as@gmail.com>
2024-07-20 10:01:57 +0300

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/app" element={<AppLayout />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
